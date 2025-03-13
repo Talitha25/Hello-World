ed4e540fc8924542409d0776f72f90299cb71aea
Talitha <akunintan@gmail.com>
2025-03-13 21:11:43 +0700

    kedua

--- src/percobaan1/Percobaan1.java
+++ src/percobaan1/Percobaan1.java
@@ -15,6 +15,7 @@ public class Percobaan1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello Wold");
     }
     
 }
